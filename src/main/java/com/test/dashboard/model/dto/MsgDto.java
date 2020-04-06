e4b1c8d966d8e5780b50be79119874ec073872d4
Jiae-M <jiae95.min@gmail.com>
2020-04-06 12:26:29 +0900

    ㅇㅇ

---
+++ src/main/java/com/test/dashboard/model/dto/MsgDto.java
@@ -0,0 +1,36 @@
+package com.test.dashboard.model.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Alias("msgDto")
+@Data
+public class MsgDto {
+	
+	// 쪽지 
+
+	    // 쪽지 고유번호 
+	    private int msgno;
+
+	    // 쪽지 발송인 
+	    private String msgfrom;
+
+	    // 쪽지 수신인 
+	    private String msgto;
+
+	    // 쪽지 보낸 날짜 
+	    private String msgdate;
+
+	    // 쪽지 개봉 여부(y/n) 
+	    private String msgopened;
+	    
+	    // 대쉬보드 번호
+	    private int dno;
+
+	    // 쪽지 내용 
+	    private String msgcontent;
+
+	    // 쪽지 제목 
+	    private String msgtitle;
+}
